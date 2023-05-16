912b8f6d53ae25952b410ce197bd26cf6dc2eea9
Gagan Vishal Mishra <gagan.vishal@mmltd.co.za>
2023-05-16 20:31:47 +0200

    Initial Commit

---
+++ UnsplashImageapp/UnsplashImageappApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnsplashImageappApp.swift
+//  UnsplashImageapp
+//
+//  Created by Gagan Vishal  on 2023/05/16.
+//
+
+import SwiftUI
+
+@main
+struct UnsplashImageappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UnsplashImageappUITests/UnsplashImageappUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  UnsplashImageappUITestsLaunchTests.swift
+//  UnsplashImageappUITests
+//
+//  Created by Gagan Vishal  on 2023/05/16.
+//
+
+import XCTest
+
+final class UnsplashImageappUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
