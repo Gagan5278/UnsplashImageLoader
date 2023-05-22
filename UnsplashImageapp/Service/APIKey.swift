e004798883ffc0cf537ee5acf8643a4d01a8ff4c
Gagan Vishal Mishra <gagan.vishal@mmltd.co.za>
2023-05-22 20:51:22 +0200

    Hide Developer API key

--- UnsplashImageapp/Service/APIKey.swift
+++ UnsplashImageapp/Service/APIKey.swift
@@ -8,5 +8,5 @@
 import Foundation
 
 enum APIKey {
-    static let unsplashAPIKey = "1327df65b8897921bf1997320633d6e8f5f7cb6aae7b6d2b06ecdcec5587e759"
+    static let unsplashAPIKey =  /***YOUR API KEY HERE  **/
 }
